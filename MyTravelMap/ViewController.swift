c52376a424ffa9a60d60c15fa71e1ae3caf5ac51
Oleksandr Roditieliev <roditelev2020@icloud.com>
2024-08-21 16:09:14 +0300

    Initial commit

---
+++ MyTravelMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTravelMap
+//
+//  Created by Александр Родителев on 19.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
